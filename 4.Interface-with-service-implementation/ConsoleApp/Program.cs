2770f59f3e79850768f47daec944f9c022f6643c
TimvandenAardweg <timvandenaardweg@me.com>
2022-07-24 14:27:07 +0200

    Register by interface and implementation

---
+++ 4.Interface-with-service-implementation/ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using DependencyInjection;
+using ConsoleApp.services;
+
+var services = new ServiceCollection();
+
+//services.RegisterSingleton(new RandomGuidGeneratorService());
+//services.RegisterSingleton<RandomGuidGeneratorService>();
+//services.RegisterTransient<RandomGuidGeneratorService>();
+services.RegisterTransient<IGuidPrintService, GuidPrintService>();
+//services.RegisterSingleton<IGuidPrintService, GuidPrintService>();
+
+Container container = services.GenerateContainer();
+
+IGuidPrintService guidService1 = container.GetService<IGuidPrintService>();
+IGuidPrintService guidService2 = container.GetService<IGuidPrintService>();
+
+guidService1.PrintGuid();
+guidService1.PrintGuid();
+guidService2.PrintGuid();
+guidService2.PrintGuid();
---
+++ 4.Interface-with-service-implementation/DependencyInjection/ServiceDescriptor.cs
@@ -0,0 +1,67 @@
+﻿namespace DependencyInjection;
+
+/// <summary>
+/// Class that describes a service.
+/// </summary>
+public class ServiceDescriptor
+{
+    /// <summary>
+    /// The lifetime of the service.
+    /// </summary>
+    public ServiceLifetime ServiceLifetime { get; }
+
+    /// <summary>
+    /// An instance of the service of type <see cref="ServiceType"/>.
+    /// </summary>
+    public object ImplementationInstance { get; internal set; }
+
+    /// <summary>
+    /// The type of the implementation.
+    /// </summary>
+    public Type ImplementationType { get; }
+
+    /// <summary>
+    /// The type of the service.
+    /// </summary>
+    public Type ServiceType { get; }
+
+    public ServiceDescriptor(object implementation, ServiceLifetime serviceLifetime)
+    {
+        if (implementation == null)
+        {
+            throw new ArgumentNullException(nameof(implementation));
+        }
+        
+        ServiceType = implementation.GetType();
+        ImplementationInstance = implementation;
+        ServiceLifetime = serviceLifetime;
+    }
+
+    public ServiceDescriptor(Type serviceType, ServiceLifetime serviceLifetime)
+    {
+        if (serviceType == null)
+        {
+            throw new ArgumentNullException(nameof(serviceType));
+        }
+        
+        ServiceType = serviceType;
+        ServiceLifetime = serviceLifetime;
+    }
+
+    public ServiceDescriptor(Type serviceType, Type implementationType, ServiceLifetime serviceLifetime)
+    {
+        if (serviceType == null)
+        {
+            throw new ArgumentNullException(nameof(serviceType));
+        }
+
+        if (implementationType == null)
+        {
+            throw new ArgumentNullException(nameof(implementationType));
+        }
+
+        ServiceLifetime = serviceLifetime;
+        ServiceType = serviceType;
+        ImplementationType = implementationType;
+    }
+}
