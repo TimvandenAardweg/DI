e7471936c0d1a0a5c9d2386ce56f71c55f218982
TimvandenAardweg <timvandenaardweg@me.com>
2022-07-24 13:41:51 +0200

    Added a different way to register singletons.

---
+++ 2.Singleton-with-service-implementation - Copy/DependencyInjection/ServiceCollection.cs
@@ -0,0 +1,73 @@
+﻿namespace DependencyInjection;
+
+/// <summary>
+/// Collection of services.
+/// </summary>
+public class ServiceCollection
+{
+    private Dictionary<Type, ServiceDescriptor> _serviceDescriptors = new Dictionary<Type, ServiceDescriptor>();
+
+    /// <summary>
+    /// Registers a new singleton service given the implementation of that service.
+    /// </summary>
+    /// <typeparam name="TService">The type of the service to register.</typeparam>
+    /// <param name="implementation">The implementation of the service.</param>
+    /// <exception cref="Exception">
+    /// Thrown when a service of type <typeparamref name="TService"/> has already been registered.
+    /// </exception>
+    public void RegisterSingleton<TService>(TService implementation) where TService : class
+    {
+        Type serviceType = typeof(TService);
+
+        if (ServiceTypeIsAlreadyRegistered(serviceType))
+        {
+            throw new Exception($"A service of type {serviceType} has already been registered.");
+        }
+
+        var serviceDescriptor = new ServiceDescriptor(implementation, ServiceLifetime.Singleton);
+
+        _serviceDescriptors.Add(typeof(TService), serviceDescriptor);
+    }
+
+    /// <summary>
+    /// Registers a new singleton service given the type of that service.
+    /// </summary>
+    /// <typeparam name="TService">The type of the service to register.</typeparam>
+    /// <exception cref="Exception">
+    /// Thrown when a service of type <typeparamref name="TService"/> has already been registered.
+    /// </exception>
+    public void RegisterSingleton<TService>() where TService : class
+    {
+        Type serviceType = typeof(TService);
+
+        if (ServiceTypeIsAlreadyRegistered(serviceType))
+        {
+            throw new Exception($"A service of type {serviceType} has already been registered.");
+        }
+
+        var serviceDescriptor = new ServiceDescriptor(serviceType, ServiceLifetime.Singleton);
+
+        _serviceDescriptors.Add(serviceType, serviceDescriptor);
+    }
+
+    /// <summary>
+    /// Creates a new container that allows access to registerd services.
+    /// </summary>
+    /// <returns></returns>
+    public Container GenerateContainer()
+    {
+        return new Container(_serviceDescriptors);
+    }
+
+    private bool ServiceTypeIsAlreadyRegistered(Type serviceType)
+    {
+        _serviceDescriptors.TryGetValue(serviceType, out var serviceDescriptor);
+
+        if (serviceDescriptor != null)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ 2.Singleton-with-service-implementation - Copy/DependencyInjection/ServiceDescriptor.cs
@@ -0,0 +1,45 @@
+﻿namespace DependencyInjection;
+
+/// <summary>
+/// Class that describes a service.
+/// </summary>
+public class ServiceDescriptor
+{
+    /// <summary>
+    /// The lifetime of the service.
+    /// </summary>
+    public ServiceLifetime ServiceLifetime { get; }
+
+    /// <summary>
+    /// An instance of the service of type <see cref="ServiceType"/>.
+    /// </summary>
+    public object ImplementationInstance { get; internal set; }
+
+    /// <summary>
+    /// The type of the service.
+    /// </summary>
+    public Type ServiceType { get; }
+
+    public ServiceDescriptor(object implementation, ServiceLifetime serviceLifeTime)
+    {
+        if (implementation == null)
+        {
+            throw new ArgumentNullException(nameof(implementation));
+        }
+        
+        ServiceType = implementation.GetType();
+        ImplementationInstance = implementation;
+        ServiceLifetime = serviceLifeTime;
+    }
+
+    public ServiceDescriptor(Type serviceType, ServiceLifetime serviceLifeTime)
+    {
+        if (serviceType == null)
+        {
+            throw new ArgumentNullException(nameof(serviceType));
+        }
+        
+        ServiceType = serviceType;
+        ServiceLifetime = serviceLifeTime;
+    }
+}
