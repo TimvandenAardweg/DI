9caff7718be8e8104102029fa68f504414c82025
TimvandenAardweg <timvandenaardweg@me.com>
2022-07-26 20:42:11 +0200

    Added initial setup for DI in WPF.

---
+++ 7.DI-in-WPF-Initial-setup/DI/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using WpfLibrary;
+
+namespace DI;
+
+public partial class MainWindow : Window
+{
+    private readonly IDataAccess dataAccess;
+
+    public MainWindow(IDataAccess dataAccess)
+    {
+        InitializeComponent();
+        this.dataAccess = dataAccess;
+    }
+
+    private void getData_Click(object sender, RoutedEventArgs e)
+    {
+        data.Text = dataAccess.GetData();
+    }
+}
